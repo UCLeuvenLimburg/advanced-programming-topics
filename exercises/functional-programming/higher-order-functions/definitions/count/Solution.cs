f011a68ca89f823458661fab5eb0d8ee0622062a
Frederic Vogels <frederic.vogels@ucll.be>
2019-07-03 16:46:51 +0200

    exercises/functional-programming/higher-order-functions/definitions/count

---
+++ exercises/functional-programming/higher-order-functions/definitions/count/Solution.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Count
+{
+    public static class Solution
+    {
+        public static int Count<T>(IEnumerable<T> xs, Func<T, bool> predicate)
+        {
+            int count = 0;
+
+            foreach (var x in xs)
+            {
+                if (predicate(x))
+                {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ exercises/functional-programming/higher-order-functions/definitions/count/UnitTest.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Count
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void OnEmptyList()
+        {
+            Check(Integers(), x => true, 0);
+        }
+
+        [Fact]
+        public void CountPositiveIntegers()
+        {
+            Check(Integers(2, -5, 3, 1, -2), x => x >= 0, 3);
+        }
+
+        [Fact]
+        public void CountOddIntegers()
+        {
+            Check(Integers(2, -5, 3, 1, -2, 8, 4), x => x % 2 != 0, 3);
+        }
+
+        [Fact]
+        public void CountEmptyStrings()
+        {
+            Check(Strings("", "x", "6q", ""), s => s.Length == 0, 2);
+        }
+
+        private static void Check<T>(IEnumerable<T> input, Func<T, bool> predicate, int expected)
+        {
+            var actual = Solution.Count<T>(input, predicate);
+
+            Assert.Equal(expected, actual);
+        }
+
+        private static List<T> Values<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+
+        private static List<int> Integers(params int[] items)
+        {
+            return Values<int>(items);
+        }
+
+        private static List<string> Strings(params string[] items)
+        {
+            return Values<string>(items);
+        }
+    }
+}
