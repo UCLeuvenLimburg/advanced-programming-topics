ef640ce05cd6c1f1bc60d3b856e777679f2c1da8
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-03 23:06:12 +0200

    find

--- exercises/functional-programming/higher-order-functions/definitions/find/Exercise/UnitTest.cs
+++ exercises/functional-programming/higher-order-functions/definitions/find/Exercise/UnitTest.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
-using Tested = Exercise.Solution;
+using Tested = Exercise.Student;
 
 namespace Exercise
 {
@@ -60,6 +60,22 @@ namespace Exercise
             IEnumerable<string> strings = new string[] { "abc", "", "QJFJKLp", "FJLKJLA", "qio" };
 
             Assert.Equal(3, Tested.IndexOf(strings, s => s.Length > 0 && s.ToUpper() == s));
+        }
+
+        [Fact]
+        public void FindFirst_Missing()
+        {
+            IEnumerable<int> ns = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            Assert.Throws<InvalidOperationException>(() => Tested.FindFirst(ns, n => n < 0));
+        }
+
+        [Fact]
+        public void IndexOf_Missing()
+        {
+            IEnumerable<int> ns = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            Assert.Throws<InvalidOperationException>(() => Tested.IndexOf(ns, n => n < 0));
         }
     }
 }
