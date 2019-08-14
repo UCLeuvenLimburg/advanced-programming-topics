44b4cb31aa1ec6058466a7ef4636249116baaed6
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-14 10:47:52 +0200

    lazy generator sample

---
+++ samples/lazy-generator/Sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xs = Bar();
+            Console.WriteLine("Looping");
+            foreach (var x in xs)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        static IEnumerable<int> Bar()
+        {
+            Console.WriteLine("A");
+            yield return 1;
+            Console.WriteLine("B");
+            yield return 2;
+            Console.WriteLine("C");
+            yield return 3;
+            Console.WriteLine("D");
+        }
+    }
+}
