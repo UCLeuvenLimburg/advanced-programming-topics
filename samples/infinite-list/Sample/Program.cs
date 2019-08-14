2adde9e7c97035d23fe2db893c9fbac2f0e78d97
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-14 10:47:37 +0200

    infinite list sample

---
+++ samples/infinite-list/Sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach ( var x in InfiniteRange() )
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        static IEnumerable<sbyte> InfiniteRange()
+        {
+            sbyte k = 0;
+
+            while ( true )
+            {
+                yield return k;
+                k++;
+            }
+        }
+    }
+}
