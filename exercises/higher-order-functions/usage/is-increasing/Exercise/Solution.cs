f2131d18517f7d8751f9acd05595d9e8fea256ca
Frederic Vogels <frederic.vogels@ucll.be>
2019-09-22 08:49:35 +0200

    is-increasing

--- exercises/higher-order-functions/usage/is-increasing/Exercise/Solution.cs
+++ exercises/higher-order-functions/usage/is-increasing/Exercise/Solution.cs
@@ -6,9 +6,14 @@ namespace Exercise.Solution
 {
     public static class IEnumerableExtensions
     {
+        public static IEnumerable<int> Deltas(this IEnumerable<int> ns)
+        {
+            return ns.Zip(ns.Skip(1), (x, y) => y - x);
+        }
+
         public static bool IsIncreasing(this IEnumerable<int> ns)
         {
-            return ns.Zip(ns.Skip(1), (x, y) => x <= y).All(x => x);
+            return ns.Deltas().All(delta => delta >= 0);
         }
     }
 }
--- exercises/higher-order-functions/usage/is-increasing/Exercise/UnitTest.cs
+++ exercises/higher-order-functions/usage/is-increasing/Exercise/UnitTest.cs
@@ -3,24 +3,81 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
-// using Exercise.Solution;
+using Exercise.Solution;
 
 namespace Exercise
 {
     public class UnitTest
     {
         [Fact]
-        public void Test()
+        public void EmptyList()
         {
-            Assert.True(new List<int> {}.IsIncreasing());
-            Assert.True(new List<int> {1}.IsIncreasing());
-            Assert.True(new List<int> {1, 2, 3, 4, 5}.IsIncreasing());
-            Assert.True(new List<int> {4, 7, 9}.IsIncreasing());
-            Assert.True(new List<int> {2, 2, 2}.IsIncreasing());
+            var list = new List<int> { };
 
-            Assert.False(new List<int> {2, 1}.IsIncreasing());
-            Assert.False(new List<int> {1, 4, 7, 8, 5}.IsIncreasing());
-            Assert.False(new List<int> {5, 7, 1, 5}.IsIncreasing());
+            Assert.True(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void SingleItem()
+        {
+            var list = new List<int> { 1 };
+            Assert.True(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void TwoIncreasingItems()
+        {
+            var list = new List<int> { 1, 2 };
+            Assert.True(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void TwoDecreasingItems()
+        {
+            var list = new List<int> { 2, 1 };
+            Assert.False(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void TwoEqualItems()
+        {
+            var list = new List<int> { 5, 5 };
+            Assert.True(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void LongStrictlyIncreasing()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            Assert.True(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void LongIncreasingWithEquals()
+        {
+            var list = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+            Assert.True(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void LongNonIncreasing()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 3, 4, 5 };
+            Assert.False(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void LongNonIncreasing2()
+        {
+            var list = new List<int> { 5, 4, 3, 2, 1 };
+            Assert.False(list.IsIncreasing());
+        }
+
+        [Fact]
+        public void ManyEqualItems()
+        {
+            var list = new List<int> { 3, 3, 3, 3, 3, 3, 3, 3 };
+            Assert.True(list.IsIncreasing());
         }
     }
 }
