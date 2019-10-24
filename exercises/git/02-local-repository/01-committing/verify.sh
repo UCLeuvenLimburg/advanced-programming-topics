126775e67e2cc7a2aeb076e7e497dc7faf8688cf
Frederic Vogels <frederic.vogels@ucll.be>
2019-10-24 12:53:56 +0200

    committing

--- exercises/git/02-local-repository/01-committing/verify.sh
+++ exercises/git/02-local-repository/01-committing/verify.sh
@@ -2,10 +2,10 @@
 
 pushd sandbox > /dev/null
 
-if [[ $(git log --format=%s) == "Initial commit" ]]; then
-  echo ok
-else
+if [[ $(git log --format=%s) != "Initial commit" ]]; then
   echo fail 
+else
+  echo ok
 fi
 
 popd > /dev/null
