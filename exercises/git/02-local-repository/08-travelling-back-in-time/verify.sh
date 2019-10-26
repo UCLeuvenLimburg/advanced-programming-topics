7e6c7600234c46ba091e992ecd59a6a52ddb5e1a
Frederic Vogels <frederic.vogels@ucll.be>
2019-10-26 21:40:44 +0200

    exercises/git/02-local-repository/08-travelling-back-in-time

--- exercises/git/02-local-repository/08-travelling-back-in-time/verify.sh
+++ exercises/git/02-local-repository/08-travelling-back-in-time/verify.sh
@@ -2,18 +2,14 @@
 
 pushd sandbox > /dev/null
 
-if [[ $(git log --format=oneline | wc -l) == 3 ]]; then
-  if [[ $(git log --format=oneline master | wc -l) == 4 ]]; then
-    if [[ $(git rev-parse HEAD) == $(git rev-parse master~1) ]]; then
-      echo ok
-    else
-      echo fail: HEAD not pointing to master~1
-    fi
-  else
-    echo fail: master branch should count four commits
-  fi
-else
+if [[ $(git log --format=oneline | wc -l) != 3 ]]; then
   echo fail: git log should return three commits
+elif [[ $(git log --format=oneline master | wc -l) != 4 ]]; then
+  echo fail: master branch should count four commits
+elif [[ $(git rev-parse HEAD) != $(git rev-parse master~1) ]]; then
+  echo fail: HEAD not pointing to master~1
+else
+  echo ok
 fi
 
 popd > /dev/null
