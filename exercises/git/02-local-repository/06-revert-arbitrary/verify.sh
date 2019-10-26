28dd1d6ea8b20610fffcfccf6f3f881f53365e06
Frederic Vogels <frederic.vogels@ucll.be>
2019-10-26 21:42:01 +0200

    exercises/git/02-local-repository/06-revert-arbitrary

--- exercises/git/02-local-repository/06-revert-arbitrary/verify.sh
+++ exercises/git/02-local-repository/06-revert-arbitrary/verify.sh
@@ -2,10 +2,10 @@
 
 pushd sandbox > /dev/null
 
-if [[ $(git ls-files | grep a.txt) && $(git ls-files | grep b.txt) && $(git ls-files | grep d.txt) && ! $(git ls-files | grep c.txt) && $(git log --format=oneline | wc -l) == 5 ]]; then
-  echo ok
-else
+if [[ $(git ls-files | grep a.txt) && $(git ls-files | grep b.txt) && $(git ls-files | grep d.txt) && ! $(git ls-files | grep c.txt) && $(git log --format=oneline | wc -l) != 5 ]]; then
   echo fail
+else
+  echo ok
 fi
 
 popd > /dev/null
