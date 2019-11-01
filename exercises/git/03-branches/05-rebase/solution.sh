158571b9f1cf9882215f5d13712fbae2ed78d8cb
Frederic Vogels <frederic.vogels@ucll.be>
2019-11-01 14:26:47 +0100

    exercises/git/03-branches/05-rebase

--- exercises/git/03-branches/05-rebase/solution.sh
+++ exercises/git/03-branches/05-rebase/solution.sh
@@ -5,6 +5,9 @@ pushd sandbox
 git checkout feature
 git rebase master
 git checkout master
+
+# --ff-only indicates that merging should only succeed if no merge commit is necessary
+# This should be the case when rebasing
 git merge --ff-only feature
 
 popd
